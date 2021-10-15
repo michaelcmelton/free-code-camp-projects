bc91805b02409cb62c78f693848396146cb30183
michaelcmelton <michaelcmelton93@gmail.com>
2021-10-14 22:55:09 -0400

    Update root api call

--- apis-and-microservices/timestamp-microservice/src/app.service.ts
+++ apis-and-microservices/timestamp-microservice/src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      message:
+        "Valid paths are served at `/api/:timestamp`. the timestamp param should be formatted as a unix timecode or '2015-06-24'",
+    };
   }
 }
